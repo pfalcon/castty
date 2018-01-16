c26fabcbdced258333d2495cc8f7c9f0b71012aa
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-01-16 23:55:38 +0200

    ui: Add sample of asciicast with annotations.

---
+++ ui/asciicast-annot.js
@@ -0,0 +1,32 @@
+// This is normal asciinema v1 recording with "var data =" line prepended.
+var data =
+{
+  "duration": 10.0,
+  "env": {
+    "TERM": "xterm",
+    "SHELL": "/bin/bash"
+  },
+  "version": 1,
+  "title": "dumb sample asciicast",
+  "height": 24,
+  "command": null,
+  "width": 80,
+  "annotations": [
+    {at:0.5, dur: 1, relx: 0.1, rely: 0.1, text: "annotation #1"},
+    {at:3.0, dur: 3, relx: 0.8, rely: 0.8, text: "annotation\n#2"}
+  ],
+  "stdout": [
+    [
+      0,
+      "Just"
+    ],
+    [
+      0.5,
+      " a dumb"
+    ],
+    [
+      3.0,
+      " sample asciicast.\r\n"
+    ],
+  ]
+}
